4d26e7247e7fee2fb29e1509c28f42eec94ec386
Marion <contact@marioncasters.fr>
2026-07-12 00:15:54 +0200

    Fix error connexion on prod env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,18 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import { relations } from "./relations";
+import mysql from "mysql2/promise";
+import "dotenv/config";
 
-export const db = drizzle(process.env.DATABASE_URL!, {
+const connection = await mysql.createConnection(process.env.DATABASE_URL!);
+export const db = drizzle({ client: connection, relations });
+
+/*export const db = drizzle(process.env.DATABASE_URL!, {
   relations,
-  logger: false,
+  logger: true,
 });
+
+export const db = drizzle({
+  connection: { uri: process.env.DATABASE_URL },
+  relations,
+  logger: true,
+});*/
