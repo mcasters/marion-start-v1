dab61d14ea4160c9ede662e34b6dcf918d8651f6
Marion <contact@marioncasters.fr>
2026-07-13 08:13:01 +0200

    Drizzle rc version

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: process.env.SCHEMA_FILE!,
   dialect: "mysql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
